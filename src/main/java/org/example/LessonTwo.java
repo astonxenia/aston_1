177bbc23fee02079a6768595bac7e6683bd16ca4
astonxenia <xeniabagriy@gmail.com>
2025-11-01 02:20:37 +0300

    created printColor

--- src/main/java/org/example/LessonTwo.java
+++ src/main/java/org/example/LessonTwo.java
@@ -16,9 +16,20 @@ public class LessonTwo {
             System.out.println("Сумма отрицательная");
         }
     }
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0  && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
 
     public static void main(String [] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 }
