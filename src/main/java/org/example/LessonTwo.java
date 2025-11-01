77cd4ce0943b696b7c3e028f33616359756b9cae
astonxenia <xeniabagriy@gmail.com>
2025-11-01 04:45:02 +0300

    created doubleNumbersBelowSix

--- src/main/java/org/example/LessonTwo.java
+++ src/main/java/org/example/LessonTwo.java
@@ -70,6 +70,15 @@ public class LessonTwo {
         }
         System.out.println(Arrays.toString(arr));
     }
+    public static void doubleNumbersBelowSix() {
+        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] <  6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
     public static void main(String [] args) {
         checkSumSign();
@@ -92,5 +101,6 @@ public class LessonTwo {
         System.out.println(isYearLeap(year));
         changeNumbersArray();
         createEmptyArray();
+        doubleNumbersBelowSix();
     }
 }
