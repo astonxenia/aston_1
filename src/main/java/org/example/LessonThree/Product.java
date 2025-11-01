bb505fac98202747f6c3a5c0e9a6f7deb83dbacf
astonxenia <xeniabagriy@gmail.com>
2025-11-01 18:19:16 +0300

    created branch Lesson_3, created Product

---
+++ src/main/java/org/example/LessonThree/Product.java
@@ -0,0 +1,35 @@
+package org.example.LessonThree;
+
+import java.util.Date;
+
+public class Product {
+        String name;
+        Date data;
+        String countryFrom;
+        String countryMade;
+        double price;
+        char order;
+        public Product(String name, Date data, String countryFrom, String countryMade, double price, char order) {
+            this.name = name;
+            this.data = data;
+            this.countryFrom = countryFrom;
+            this.countryMade = countryMade;
+            this.price = price;
+            this.order = order;
+        }
+
+        public void printInfo() {
+            System.out.println("название: " + name + "\n" +
+                    "дата производства: " + data + "\n" +
+                    "производитель: " + countryFrom + "\n" +
+                    "страна происхождения: " + countryMade + "\n" +
+                    "цена: " + price + "\n" +
+                    "состояние бронирование покупателем: " + order);
+        }
+
+        public static void main(String[] args) {
+            Product product = new Product("Масло", new Date(), "Россия",
+                    "Беларусь", 250.50, 'N');
+            product.printInfo();
+        }
+    }
