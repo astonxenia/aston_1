514297bf86dd9b097924211658b866e071fbc7f1
astonxenia <xeniabagriy@gmail.com>
2025-11-05 19:09:36 +0300

    created List of attractins

--- src/main/java/org/example/LessonThree/Park.java
+++ src/main/java/org/example/LessonThree/Park.java
@@ -22,12 +22,14 @@ public class Park {
             this.time = time;
             this.price = price;
         }
+
         public void printInfo() {
             System.out.println("Аттракцион: " + name);
             System.out.println("Время работы: " + time);
             System.out.println("Цена: " + price);
         }
     }
+
     public void addAttraction(String name, String time, double price)  {
         Attraction attraction = new Attraction(name, time, price);
         attractions.add(attraction);
