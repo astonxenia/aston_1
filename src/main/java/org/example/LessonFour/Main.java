6c1b647b7307c16a4c0c8f37e937f4028044e445
astonxenia <xeniabagriy@gmail.com>
2025-11-08 06:28:55 +0300

    changed Main

--- src/main/java/org/example/LessonFour/Main.java
+++ src/main/java/org/example/LessonFour/Main.java
@@ -1,7 +1,5 @@
 package org.example.LessonFour;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Shape circle = new Circle(10, "Зеленый", "Оранжевый");
