f989116f725b7c0d5fc91a7b9930b205a1796b98
astonxenia <xeniabagriy@gmail.com>
2025-11-08 05:46:45 +0300

    added Rectangle

--- src/main/java/org/example/LessonFour/Rectangle.java
+++ src/main/java/org/example/LessonFour/Rectangle.java
@@ -1,4 +1,35 @@
 package org.example.LessonFour;
 
-public class Rectangle {
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+    private String fillColour;
+    private String borderColour;
+
+    public Rectangle(double width, double height, String fillColour, String borderColour) {
+        this.width = width;
+        this.height = height;
+        this.fillColour = fillColour;
+        this.borderColour = borderColour;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + height) * 2;
+    }
+
+    @Override
+    public String getFillColour() {
+        return fillColour;
+    }
+
+    @Override
+    public String getBorderColour() {
+        return borderColour;
+    }
 }
