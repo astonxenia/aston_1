3b356429485b3412d4cb106790f228bcefcdbdda
astonxenia <xeniabagriy@gmail.com>
2025-11-08 03:40:22 +0300

    created files

--- src/main/java/org/example/LessonFour/Animal.java
+++ src/main/java/org/example/LessonFour/Animal.java
@@ -1,4 +1,22 @@
 package org.example.LessonFour;
 
-public class Animal {
+public abstract class Animal {
+    String name;
+    protected static int count = 0;
+
+    public Animal (String name) {
+        this.name = name;
+        count++;
+    }
+
+    public void animalInfo() {
+        System.out.println("Кличка животного: " + name);
+    }
+
+    public abstract void run(int distance);
+    public abstract void swim(int distance);
+
+    public static int getCount() {
+        return count;
+    }
 }
