dd691c1ed917ac2a045c07ba5a4102533bd91f97
astonxenia <xeniabagriy@gmail.com>
2025-11-15 18:27:10 +0300

    add StudentsSorting

--- src/main/java/org/example/LessonSix/Student/StudentsSorting.java
+++ src/main/java/org/example/LessonSix/Student/StudentsSorting.java
@@ -1,4 +1,36 @@
 package org.example.LessonSix.Student;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class StudentsSorting {
+    private Set<Student> students;
+
+    public StudentsSorting () {
+       this.students = new HashSet<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void deleteLowScoringStudents() {
+        students.removeIf(s -> s.getAverageGrade() < 3);
+    }
+
+    public void toNextCourse() {
+        for(Student s : students) {
+            if(s.getAverageGrade() >= 3) s.nextCourse();
+        }
+    }
+
+    public void printStudents(int course) {
+        System.out.println("Студенты " + course + " курса");
+        for(Student s : students) {
+            if(s.getCourse() == course) {
+                System.out.println(s.getName());
+            }
+        }
+    }
 }
