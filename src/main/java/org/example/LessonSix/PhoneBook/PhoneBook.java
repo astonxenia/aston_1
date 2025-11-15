83c5c9ceb5ed34db3d4234ead79a5a7c0557bce2
astonxenia <xeniabagriy@gmail.com>
2025-11-15 19:20:16 +0300

    created PhoneBook class

--- src/main/java/org/example/LessonSix/PhoneBook/PhoneBook.java
+++ src/main/java/org/example/LessonSix/PhoneBook/PhoneBook.java
@@ -1,4 +1,18 @@
 package org.example.LessonSix.PhoneBook;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, List<String>> book  = new HashMap<>();
+
+    public void add(String lastName, String phoneNumber) {
+        book.computeIfAbsent(lastName, k -> new ArrayList<>()).add(phoneNumber);
+    }
+
+    public List<String> get(String lastName) {
+        return book.getOrDefault(lastName, new ArrayList<>());
+    }
 }
