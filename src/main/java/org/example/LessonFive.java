b3a0e16f68703bde52edf1c01d9bf1c684607bf7
astonxenia <xeniabagriy@gmail.com>
2025-11-15 12:27:06 +0300

    MyArraySizeException

--- src/main/java/org/example/LessonFive.java
+++ src/main/java/org/example/LessonFive.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class LessonFive {
+
+    public static void myArray(String [][] array) {
+        if(array.length  != 4) {
+            throw new MyArraySizeException("Неверное количество строк, должно быть 4");
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Неверное количество столбцов должно быть 4");
+            }
+        }
+        System.out.println("Размер массива 4Х4");
+    }
+}
+
+class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
 }
