e08fe4107153cbf1cd1f0d32e0f628a952038f25
astonxenia <xeniabagriy@gmail.com>
2025-11-15 17:43:48 +0300

    add Student

--- src/main/java/org/example/LessonSix/Student/Student.java
+++ src/main/java/org/example/LessonSix/Student/Student.java
@@ -1,4 +1,48 @@
 package org.example.LessonSix.Student;
 
+import java.util.List;
+
 public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private List<Integer> grades;
+
+    public Student(String name, String group, int course, List<Integer> grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public double getAverageGrade() {
+        if(grades == null || grades.isEmpty()) {
+            return 0.0;
+        }
+        int sum = 0;
+        for (Integer grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.size();
+    }
+
+    public void nextCourse() {
+        this.course++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
 }
