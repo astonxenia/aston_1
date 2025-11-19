7e8b9229fb1ed1d111d4f0f1af830df9541ef91e
astonxenia <xeniabagriy@gmail.com>
2025-11-19 18:08:41 +0300

    added NumberComparator

--- src/main/java/org/example/LessonSeven/NumberComparator.java
+++ src/main/java/org/example/LessonSeven/NumberComparator.java
@@ -1,4 +1,7 @@
 package org.example.LessonSeven;
 
 public class NumberComparator {
+    public static String compare(int a, int b) {
+        return (a == b) ? "Числа равны" : "Числа не равны";
+    }
 }
