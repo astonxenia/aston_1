f8bd09bd35e20de5ef722e18be0a4891cad246ed
astonxenia <xeniabagriy@gmail.com>
2025-11-19 17:16:04 +0300

    added Factorial

--- src/main/java/org/example/LessonSeven/Factorial.java
+++ src/main/java/org/example/LessonSeven/Factorial.java
@@ -1,4 +1,14 @@
 package org.example.LessonSeven;
 
 public class Factorial {
+    public static long calculateFactorial (int number){
+        if(number < 0)  {
+            throw new IllegalArgumentException("Число должно быть положительным");
+        }
+        long result = 1;
+        for (int i = 2; i <= number; i++)  {
+            result *= i;
+        }
+        return (long) result;
+    }
 }
