40aed7d6910aabe3934626a1893334d5b00aed06
astonxenia <xeniabagriy@gmail.com>
2025-11-19 17:59:30 +0300

    added ArithmeticOperations

--- src/main/java/org/example/LessonSeven/ArithmeticOperations.java
+++ src/main/java/org/example/LessonSeven/ArithmeticOperations.java
@@ -1,4 +1,19 @@
 package org.example.LessonSeven;
 
 public class ArithmeticOperations {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    public static double multiply(int a, int b) {
+        return a * b;
+    }
+    public static double division(int a, int  b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("На ноль делить нельзя");
+        }
+        return (double) a / b;
+    }
 }
