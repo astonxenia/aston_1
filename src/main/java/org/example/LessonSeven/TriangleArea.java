84a4b9d561bb2fb33ba37f60883f264bf6bd6f3b
astonxenia <xeniabagriy@gmail.com>
2025-11-19 17:42:50 +0300

    added TriangleArea

--- src/main/java/org/example/LessonSeven/TriangleArea.java
+++ src/main/java/org/example/LessonSeven/TriangleArea.java
@@ -1,4 +1,14 @@
 package org.example.LessonSeven;
 
 public class TriangleArea {
-}
+    public static double triangleArea(int sideA, int sideB, int sideC) {
+        if(sideA <= 0  || sideB <= 0 || sideC <= 0) {
+            throw new IllegalArgumentException("Длины сторон должны быть больше 0");
+        }
+        if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA)) {
+            throw new IllegalArgumentException("Таких треугольников не существует");
+        }
+        double p = (sideA + sideB + sideC) / 2.0;
+        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+    }
+    }
