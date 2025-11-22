e857eee38ff8cf5d8df4c314eaf3773bb54af78f
astonxenia <xeniabagriy@gmail.com>
2025-11-22 18:42:26 +0300

    files

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,2 +1,22 @@
+import org.example.LessonSeven.Factorial;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 public class FactorialTest {
+    @Test(description = "Вычисление факториала от нуля и выше")
+    public void testCalculateFactorial() {
+        assertEquals(1, Factorial.calculateFactorial(0));
+        assertEquals(1, Factorial.calculateFactorial(1));
+        assertEquals(120, Factorial.calculateFactorial(5));
+        assertEquals(3628800, Factorial.calculateFactorial(10));
+    }
+
+    @Test(description = "Проверка исключений при отрицательном числе",
+            expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Число должно быть положительным")
+
+    public void testCalculateFactorialNegative() {
+        Factorial.calculateFactorial(-1);
+    }
 }
