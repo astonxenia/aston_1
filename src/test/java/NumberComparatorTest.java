c02b8d92916474b53a96f0a4ebd04179cbf09054
astonxenia <xeniabagriy@gmail.com>
2025-11-22 19:25:49 +0300

    files

--- src/test/java/NumberComparatorTest.java
+++ src/test/java/NumberComparatorTest.java
@@ -1,2 +1,22 @@
+import org.example.LessonSeven.NumberComparator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class NumberComparatorTest {
+    @Test
+    public void testNumbersEqual() {
+        Assert.assertEquals(NumberComparator.compare(5, 5), "Числа равны");
+    }
+
+    @Test
+    public void testNumbersNotEqual() {
+        Assert.assertEquals(NumberComparator.compare(5, 10), "Числа не равны");
+    }
+
+    @Test
+    public void testNumbersNegativeAndZero() {
+        Assert.assertEquals(NumberComparator.compare(-1, -1), "Числа равны");
+        Assert.assertEquals(NumberComparator.compare(0, 0), "Числа равны");
+        Assert.assertEquals(NumberComparator.compare(-1, 1), "Числа не равны");
+    }
 }
