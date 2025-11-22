d58bb267a0bff9601fe4bf0042e5c70d11116ac7
astonxenia <xeniabagriy@gmail.com>
2025-11-22 17:22:07 +0300

    files

--- src/test/java/NumberComparatorTest.java
+++ src/test/java/NumberComparatorTest.java
@@ -1,2 +1,23 @@
+import org.example.LessonSeven.NumberComparator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Тест для класса NumberComparator")
+
 public class NumberComparatorTest {
-}
+
+    @ParameterizedTest(name = "{0} : {1} -> {2}")
+    @CsvSource({"1, 1, Числа равны",
+            "4, 3, Числа не равны",
+            "0, 0, Числа равны",
+    "-1, -1, Числа равны",
+    "-1, 1,  Числа не равны"})
+
+    void testCompare(int a, int b, String expected) {
+        assertEquals(expected, NumberComparator.compare(a, b));
+        }
+    }
+
