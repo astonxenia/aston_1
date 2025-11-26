db78b8cd3236fbd2ef9ab67b47a8c11019db5352
astonxenia <xeniabagriy@gmail.com>
2025-11-26 16:29:03 +0300

    added

--- src/test/java/echo/EchoPostRawTextTest.java
+++ src/test/java/echo/EchoPostRawTextTest.java
@@ -1,4 +1,24 @@
 package echo;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
 public class EchoPostRawTextTest {
+
+    @Test
+    public void testResponseIsOkAndBodyHasData() {
+        String expectedData = "This is expected to be sent back as part of response body.";
+
+        RestAssured.given()
+                .baseUri("https://postman-echo.com")
+                .body(expectedData)
+                .contentType("text/plain")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(expectedData));
+    }
 }
