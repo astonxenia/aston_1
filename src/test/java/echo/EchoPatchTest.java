f47ce1f5efb3c38bf5e66f31699af8a6162a8ad1
astonxenia <xeniabagriy@gmail.com>
2025-11-26 19:19:15 +0300

    added

--- src/test/java/echo/EchoPatchTest.java
+++ src/test/java/echo/EchoPatchTest.java
@@ -1,25 +1,5 @@
 package echo;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class EchoPatchTest {
-    @Test
-    public void testResponseIsOkAndBodyHasData() {
-        String expectedBody = "This is expected to be sent back as part of response body.";
-        Response response = RestAssured.given()
-                .log().all()
-                .baseUri("https://postman-echo.com")
-                .body(expectedBody)
-                .contentType("text/plain")
-                .when()
-                .patch("/patch");
 
-        response.then().statusCode(200);
-        String actualData = response.path("data");
-        assertEquals(expectedBody, actualData);
-    }
 }
--- src/test/java/echo/EchoPostFromDataTest.java
+++ src/test/java/echo/EchoPostFromDataTest.java
@@ -1,25 +1,5 @@
 package echo;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-
 public class EchoPostFromDataTest {
 
-    @Test
-    public void testFromDataIsResponse() {
-        RestAssured
-                .given().log().all()
-                .baseUri("https://postman-echo.com")
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .formParam("foo1", "bar1")
-                .formParam("foo2","bar2")
-                .when()
-                .post("/post")
-                .then()
-                .statusCode(200)
-                .body("form.foo1", equalTo("bar1"))
-                .body("form.foo2", equalTo("bar2"));
-    }
 }
