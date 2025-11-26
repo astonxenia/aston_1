da52ee0654b5619e748df5a4d4278e8943fa5409
astonxenia <xeniabagriy@gmail.com>
2025-11-26 16:14:26 +0300

    added

---
+++ src/test/java/echo/EchoGetTest.java
@@ -0,0 +1,27 @@
+package echo;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class EchoGetTest {
+
+        @Test
+        public void testGetRequest() {
+            RestAssured
+                    .given()
+                    .baseUri("https://postman-echo.com")
+                    .queryParam("foo1", "bar1")
+                    .queryParam("foo2","bar2")
+                    .when()
+                    .get("/get")
+                    .then()
+                    .statusCode(200)
+                    .body("args.foo1", equalTo("bar1"))
+                    .body("args.foo2", equalTo("bar2"));
+
+        }
+
+    }
+
