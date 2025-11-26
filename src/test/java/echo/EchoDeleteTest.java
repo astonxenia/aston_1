ee46cfd8aba99535f37255f9f9a103d900d3a530
astonxenia <xeniabagriy@gmail.com>
2025-11-26 17:28:39 +0300

    added

--- src/test/java/echo/EchoDeleteTest.java
+++ src/test/java/echo/EchoDeleteTest.java
@@ -1,4 +1,26 @@
 package echo;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EchoDeleteTest {
+
+    @Test
+    public void testDeleteResponseBody() {
+        Response response = RestAssured.given()
+                .log().all()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .delete("/delete");
+        response.then().statusCode(200);
+        String dataResponse = response.path("data");
+        assertEquals("This is expected to be sent back as part of response body.", dataResponse);
+
+
+    }
 }
